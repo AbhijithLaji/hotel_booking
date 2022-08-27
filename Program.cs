74e0cf0ad20692da39b7c6f356949ee3d86d60e2
USER <USER@DESKTOP-VBD4IJC>
2022-08-27 11:00:11 +0530

    API updated

--- Program.cs
+++ Program.cs
@@ -7,123 +7,104 @@ builder.Services.AddCors(option=>option.AddDefaultPolicy(
     .AllowAnyHeader()
     .AllowAnyMethod()
 ));
-builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
-builder.Services.AddDbContext<DataDb>(opt => opt.UseInMemoryDatabase("DataList"));
+builder.Services.AddDbContext<HotelDb>(opt => opt.UseInMemoryDatabase("HotelDb"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 app.UseCors();
-app.MapGet("/rooms", async (TodoDb db) =>
-    await db.Todos.ToListAsync());
 
+app.MapGet("/",()=>"Hello World");
 
+app.MapGet("/rooms", async (HotelDb db) =>
+    await db.Rooms.Include(r=>r.Bookings).ToListAsync());
 
-app.MapPost("/rooms", async (Todo todo, TodoDb db) =>
+app.MapGet("/room/{Id}",
+    async(int Id,HotelDb db) => 
+    await db.Rooms.Include(r=>r.Bookings)
+    .Where(r=>r.Id == Id).SingleOrDefaultAsync()
+);
+
+app.MapPost("/room", async (Room room, HotelDb db) =>
 {
-    db.Todos.Add(todo);
+    db.Rooms.Add(room);
     await db.SaveChangesAsync();
-
-    return Results.Created($"/rooms/{todo.Id}", todo);
+    return Results.Created($"/rooms/{room.Id}", room);
 });
 
 
-app.MapPut("/rooms/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+app.MapPut("/room/{id}", async (int id, Room roomToUpdate, HotelDb db) =>
 {
-    var todo = await db.Todos.FindAsync(id);
+    var room = await db.Rooms.FindAsync(id);
 
-    if (todo is null) return Results.NotFound();
+    if (room is null) return Results.NotFound();
 
-    todo.roomno = inputTodo.roomno;
-    todo.adultno = inputTodo.adultno;
-    todo.childno = inputTodo.childno;
-    todo.price = inputTodo.price;
+    room.RoomNo = roomToUpdate.RoomNo;
+    room.AdultCapacity = roomToUpdate.AdultCapacity;
+    room.ChildCapacity = roomToUpdate.ChildCapacity;
+    room.Price = roomToUpdate.Price;
 
     await db.SaveChangesAsync();
 
-    return Results.NoContent();
+    return Results.Ok();
 });
 
-app.MapDelete("/rooms/{id}", async (int id, TodoDb db) =>
+app.MapDelete("/room/{id}", async (int id, HotelDb db) =>
 {
-    if (await db.Todos.FindAsync(id) is Todo todo)
+    if (await db.Rooms.FindAsync(id) is Room room)
     {
-        db.Todos.Remove(todo);
+        db.Rooms.Remove(room);
         await db.SaveChangesAsync();
-        return Results.Ok(todo);
+        return Results.Ok(room);
     }
 
     return Results.NotFound();
 });
+
 //booking
-app.MapGet("/booking", async (DataDb db) =>
-    await db.Datas.ToListAsync());
-app.MapPost("/booking", async (Data Data, DataDb db) =>
+app.MapGet("/bookings", async (HotelDb db) =>
+    await db.Bookings.ToListAsync());
+
+app.MapPost("/booking", async (Booking booking, HotelDb db) =>
 {
-    db.Datas.Add(Data);
+    var room = await db.Rooms.FindAsync(booking.RoomId);
+    if(room is null) return Results.NotFound();
+    room.Bookings.Add(booking);
     await db.SaveChangesAsync();
-
-    return Results.Created($"/booking/{Data.Id}", Data);
+    return Results.Created($"/booking/{booking.Id}", booking);
 });
-app.MapPut("/booking/{id}", async (int id, Data inputData, DataDb db) =>
-{
-    var data = await db.Datas.FindAsync(id);
 
-    if (data is null) return Results.NotFound();
-
-    data.lastname = inputData.lastname;
-    data.firstname = inputData.firstname;
-    data.checkedin = inputData.checkedin;
-    data.checkedout = inputData.checkedout;
-    data.adultnumber = inputData.adultnumber;
-    data.childnumber = inputData.childnumber;
-    
-
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-});
-app.MapDelete("/booking/{id}", async (int id, DataDb db) =>
-{
-    if (await db.Datas.FindAsync(id) is Data data)
-    {
-        db.Datas.Remove(data);
-        await db.SaveChangesAsync();
-        return Results.Ok(data);
-    }
-    return Results.NotFound();
-});
 app.Run();
 
-class Todo
+class Room
+{
+    public Room(){
+        Bookings = new List<Booking>();
+        CreatedAt = DateTime.Now;
+    }
+    public int Id {get; set;}
+    public int RoomNo { get; set; }
+    public int AdultCapacity { get; set; }
+    public int ChildCapacity {get; set;}
+    public int Price {get; set;}
+    public DateTime CreatedAt { get; internal set; }
+    public ICollection<Booking> Bookings { get; set; }
+}
+class Booking
 {
     public int Id {get; set;}
-    public int roomno { get; set; }
-    public int adultno { get; set; }
-    public int childno {get; set;}
-    public int price {get; set;}
-}
-class Data
-{
-    public int Id {get; set;}
-    public string? lastname {get; set;}
-    public string? firstname { get; set; }
-    public string? checkedin { get; set; }
-    public string? checkedout {get; set;}
-    public int adultnumber {get; set;}
-    public int childnumber {get; set;}
-
+    public string? LastName {get; set;}
+    public string? FirstName { get; set; }
+    public DateTime CheckedInDate { get; set; }
+    public DateTime CheckedOutDate {get; set;}
+    public int AdultNumber {get; set;}
+    public int ChildNumber {get; set;}
+    public int RoomId { get; set; }
 }
 
-class TodoDb : DbContext
+class HotelDb : DbContext
 {
-    public TodoDb(DbContextOptions<TodoDb> options)
+    public HotelDb(DbContextOptions<HotelDb> options)
         : base(options) { }
 
-    public DbSet<Todo> Todos => Set<Todo>();
-}
-class DataDb : DbContext
-{
-    public DataDb(DbContextOptions<DataDb> options)
-        : base(options) { }
-
-    public DbSet<Data> Datas => Set<Data>();
+    public DbSet<Room> Rooms => Set<Room>();
+    public DbSet<Booking> Bookings => Set<Booking>();
 }
